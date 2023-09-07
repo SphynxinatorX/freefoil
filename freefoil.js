016354ca96af498c4ca22a46932c173d1ec7af30
SphynxinatorX <sley6042@gmail.com>
2023-09-08 00:23:17 +1000

    Simplfy things, add validation

--- freefoil.js
+++ freefoil.js
@@ -1,33 +1,23 @@
-let credentialsJson = "";
-let clientID = "";
-let clientSecret = "";
-let authCode = "";
-
-let openOauthButton = undefined;
-let generateCodeButton = undefined;
-let generateTokenButton = undefined;
+let credentialsJson, clientID, clientSecret, authCode;
+let openOauthButton, generateTokenButton, authURLInput, authURLlabel;
 
 document.addEventListener("DOMContentLoaded", (e) => {
   openOauthButton = document.querySelector("#openAuth");
-  generateCodeButton = document.querySelector("#generateCode");
   generateTokenButton = document.querySelector("#generateToken");
-  let authURLInput = document.querySelector("#authURL");
+  authURLInput = document.querySelector("#authURL");
+  authURLlabel = document.querySelector(".authURLValid");
 
   openOauthButton.addEventListener("click", (e) => {
     oauthSignIn();
-  });
-
-  generateCodeButton.addEventListener("click", (e) => {
-    getCode();
   });
 
   generateTokenButton.addEventListener("click", (e) => {
     getAuth();
   });
 
-  authURLInput.addEventListener('keyup', e => {
-    generateCodeButton.disabled = false;
-  })
+  authURLInput.addEventListener("keyup", (e) => {
+    getCode();
+  });
 
   const fileSelector = document.getElementById("file-selector");
   fileSelector.addEventListener("change", (e) => {
@@ -35,38 +25,26 @@ document.addEventListener("DOMContentLoaded", (e) => {
   });
 });
 
-readFile = (e) => {
-  let file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  let reader = new FileReader();
-  reader.onload = function (e) {
-    let contents = e.target.result;
-
-    credentialsJson = JSON.parse(contents);
-    clientID = credentialsJson.installed.client_id;
-    clientSecret = credentialsJson.installed.client_secret;
-
-    document.querySelector("#clientID").value = clientID;
-    document.querySelector("#clientSecret").value = clientSecret;
-
-    openOauthButton.disabled = false;
-  };
-  reader.readAsText(file);
-};
-
 getCode = () => {
-  let string = document.querySelector("#authURL").value;
-  let code = string.split("&code=").pop().split("&scope=")[0];
-  authCode = code;
-  document.querySelector("#authCode").value = code;
-  generateTokenButton.disabled = false;
+  let successURL = document.querySelector("#authURL").value;
+  match = successURL.match("4/[0-9A-Za-z-_]+");
+
+  if (match) {
+    authCode = successURL.match("4/[0-9A-Za-z-_]+")[0];
+    generateTokenButton.disabled = false;
+    authURLlabel.innerText = "Valid";
+    authURLlabel.style.color = "green";
+  } else {
+    authURLlabel.innerText = "Invalid";
+    authURLlabel.style.color = "red";
+  }
 };
 
 oauthSignIn = () => {
   window.open(
-    `https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?client_id=${clientID}&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&include_granted_scopes=true&state=pass-through%20value&access_type=offline&service=lso&o2v=2&flowName=GeneralOAuthFlow`
+    `https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?client_id=${clientID}&redirect_uri=http://localhost:8080&response_type=code&scope=https://www.googleapis.com/auth/drive&include_granted_scopes=true&state=pass-through value&access_type=offline&service=lso&o2v=2&flowName=GeneralOAuthFlow`,
+    "popup",
+    "popup=true,width=400,height=600"
   );
 };
 
@@ -93,32 +71,57 @@ getAuth = () => {
     .then((result) => {
       resultsJSON = JSON.parse(result);
 
+      if (resultsJSON.error) {
+        alert(
+          "Generation failed! Ensure that all other steps have been followed, and that you aren't trying to use a link that's already been used to generate a file."
+        );
+        return false;
+      }
+
       let gdriveToken = {
         access_token: resultsJSON.access_token,
         refresh_token: resultsJSON.refresh_token,
       };
 
-      var filename = "gdrive.token";
-
+      let filename = "gdrive.token";
       download(filename, gdriveToken);
     })
     .catch((error) => console.log("error", error));
 };
 
+readFile = (e) => {
+  let file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  let reader = new FileReader();
+  reader.onload = function (e) {
+    let contents = e.target.result;
+
+    credentialsJson = JSON.parse(contents);
+    clientID = credentialsJson.installed.client_id;
+    clientSecret = credentialsJson.installed.client_secret;
+
+    document.querySelector("#clientID").value = clientID;
+    document.querySelector("#clientSecret").value = clientSecret;
+
+    openOauthButton.disabled = false;
+  };
+  reader.readAsText(file);
+};
+
 download = (filename, text) => {
   text = JSON.stringify(text);
 
-  var element = document.createElement("a");
+  let element = document.createElement("a");
   element.setAttribute(
     "href",
     "data:text/plain;charset=utf-8," + encodeURIComponent(text)
   );
   element.setAttribute("download", filename);
-
   element.style.display = "none";
   document.body.appendChild(element);
 
   element.click();
-
   document.body.removeChild(element);
 };
